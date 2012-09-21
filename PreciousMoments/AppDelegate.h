7f3ae4cbeda60bc9fe26e0150b03a58f700bdbbe
LingYun <lingyun583727139@163.com>
2012-09-21 18:47:34 +0800

    Initial Commit

---
+++ PreciousMoments/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PreciousMoments
+//
+//  Created by LingYun on 12-9-21.
+//  Copyright (c) 2012年 LingYun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
